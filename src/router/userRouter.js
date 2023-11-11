4dcbf5e017b98f47e113fc65ccb9d8351d73455b
Nontapat <65050434@kmitl.ac.th>
2023-11-12 05:51:37 +0700

    add login and user page

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -8,7 +8,7 @@ userRouter.route("/").get((req, res) => {
     res.render("index");
 });
 
-userRouter.route("/userlink").get(async (req, res) => {
+userRouter.route("/username").get(async (req, res) => {
     if (!req.body) {
         res.status(400).json({ error: "ค่า req.body ไม่ถูกต้อง" });
         return;
@@ -16,7 +16,7 @@ userRouter.route("/userlink").get(async (req, res) => {
     try {
         const username = req.query.username;
         await upsertDataByName("dailyplanner", "users", { username: username }, req.query);
-        res.render("userlink", req.query);
+        res.redirect("./" + username);
     } catch (error) {
         console.error("เกิดข้อผิดพลาด:", error);
         res.status(500).json({ error: "เกิดข้อผิดพลาดในการอัปเดตข้อมูล" });
@@ -25,41 +25,35 @@ userRouter.route("/userlink").get(async (req, res) => {
 
 userRouter.route("/:username").get(async (req, res) => {
     const username = req.params.username;
-    res.render("user", {username: username});
+    res.render("user", { username: username });
 });
 
-userRouter.route("/:username/create").post(async (req, res) => {
+userRouter.route("/:username/planner").get(async (req, res) => {
+    const date = req.query.date;
+    res.redirect("./" + date);
+});
+
+userRouter.route("/:username/planner").post(async (req, res) => {
     if (!req.body) {
         res.status(400).json({ error: "ค่า req.body ไม่ถูกต้อง" });
         return;
     }
     try {
         const username = req.params.username;
+        const date = req.body.date;
         req.body.username = username;
-        const { date } = req.body;
-        await upsertDataByName("dailyplanner", "planners", { date: date, username: username}, req.body);
+        await upsertDataByName("dailyplanner", "planners", { date: date, username: username }, req.body);
+        res.redirect("./" + date);
     } catch (error) {
         console.error("เกิดข้อผิดพลาด:", error);
         res.status(500).json({ error: "เกิดข้อผิดพลาดในการอัปเดตข้อมูล" });
     }
 });
 
-userRouter.route("/:username/:date").post(async (req, res) => {
-    if (!req.body) {
-        res.status(400).json({ error: "ค่า req.body ไม่ถูกต้อง" });
-        return;
-    }
-    try {
-        console.log(req.body);
-        const username = req.params.username;
-        const date = req.params.date;
-        req.body.username = username;
-        await upsertDataByName("dailyplanner", "planners", { date: date, username: username}, req.body);
-        res.render("planner", {username: username, date: date});
-    } catch (error) {
-        console.error("เกิดข้อผิดพลาด:", error);
-        res.status(500).json({ error: "เกิดข้อผิดพลาดในการอัปเดตข้อมูล" });
-    }
+userRouter.route("/:username/:date").get(async (req, res) => {
+    const username = req.params.username;
+    const date = req.params.date;
+    res.render("planner", { date: date, username: username });
 });
 
 module.exports = userRouter;
